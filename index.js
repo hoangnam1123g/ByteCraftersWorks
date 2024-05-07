1880f045fee7ac6afbabf5d4944eeece78eddb96
hoangnam1123g <brenden.vcpun0suy1@hotmail.com>
2024-05-07 12:01:20 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const shellSort = (arr) => {
-  const n = arr.length;
-  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
-    for (let i = gap; i < n; i++) {
-      const temp = arr[i];
-      let j;
-      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-        arr[j] = arr[j - gap];
-      }
-      arr[j] = temp;
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
   }
-  return arr;
-};
+  return dp[m - 1][n - 1];
+}
